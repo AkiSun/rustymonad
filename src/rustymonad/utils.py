bf1c7f8c00b39cff351b5014dac5bbaef3ac5ffb
AkiSun <449013949@qq.com>
2024-09-15 12:28:24 +0800

    refactor: improve type hints for utils

--- src/rustymonad/utils.py
+++ src/rustymonad/utils.py
@@ -1,17 +1,19 @@
 from functools import wraps
-from typing import TypeVar, Callable, Generator, TypeAlias, Any
+from typing import TypeVar, Callable, Generator, TypeAlias, ParamSpec, Any
 from types import GeneratorType
 from .monad import Monad
 from .result import Result, Ok, Err
 
 
+P = ParamSpec('P')
 T = TypeVar('T')
-DoRet: TypeAlias = Generator[Monad, Any, T]
+M = TypeVar('M', bound=Monad)
+DoRet: TypeAlias = Generator[Monad, Any, M]
 
 
-def do_notation(func: Callable[..., DoRet[Monad]]) -> Callable[..., Monad]:
+def do_notation(func: Callable[P, DoRet[M]]) -> Callable[P, M]:
     @wraps(func)
-    def _wrapper(*args, **kwargs) -> Monad:
+    def _wrapper(*args: P.args, **kwargs: P.kwargs) -> M:
         generator = func(*args, **kwargs)
         if isinstance(generator, GeneratorType):
             monad = Monad(None)
